15e7e4b4feda75597c4211be60983aad4ca505be
Shakti Mishra <mshakti49@gmail.com>
2025-09-17 14:19:47 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -206,12 +206,12 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "WhatsApp Image 2025-09-17 at 12.18.09 AM.jpeg",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "Feel free to reach out for CRM, cloud, testing, or operations roles. Open to internships and freelance opportunities — I reply within 24 hours.",
   },
   blogSection: {
     title: "Blogs",
     subtitle:
-      "I like to document some of my experiences in professional career journey as well as some technical knowledge sharing.",
+      "I document my professional journey and technical learnings.",
     link: "https://blogs.Shakti.com/",
     avatar_image_path: "WhatsApp Image 2025-09-17 at 12.18.09 AM.jpeg",
   },
