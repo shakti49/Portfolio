e28dc5f5717f4f979352870ce74c9394c0dd365e
Shakti Mishra <mshakti49@gmail.com>
2025-10-15 21:50:32 +0530

    Replace 'Ashutosh' with 'Shakti Mishra' in SkillSection.js

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -28,14 +28,12 @@ class SkillSection extends Component {
             <div key={i} className="skills-main-div">
               <Fade left duration={2000}>
                 <div className="skills-image-div">
-                  {/* <img
-                    alt="Ashutosh is Analysing Data"
+                  {/*                     alt="Shakti Mishra is Analysing Data"
                     src={require(`../../assets/images/${skill.imagePath}`)}
-                  ></img> */}
+                  > */}
                   <GetSkillSvg fileName={skill.fileName} theme={theme} />
                 </div>
               </Fade>
-
               <div className="skills-text-div">
                 <Fade right duration={1000}>
                   <h1 className="skills-heading" style={{ color: theme.text }}>
