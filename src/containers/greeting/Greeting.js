30a40bccc1d495dfd2e24597cf4867c352aab1d8
Shakti Mishra <mshakti49@gmail.com>
2025-10-15 22:47:31 +0530

    Change name to h1 with main-name class

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -7,52 +7,49 @@ import { Fade } from "react-reveal";
 import FeelingProud from "./FeelingProud";
 
 export default function Greeting(props) {
-  const theme = props.theme;
-  return (
-    <Fade bottom duration={2000} distance="40px">
-      <div className="greet-main" id="greeting">
-        <div className="greeting-main">
-          <div className="greeting-text-div">
-            <div>
-              <h1 className="greeting-text" style={{ color: theme.text }}>
-                {greeting.title}
-              </h1>
-              {greeting.nickname && (
-                <h2 className="greeting-nickname" style={{ color: theme.text }}>
-                  ( {greeting.nickname} )
-                </h2>
-              )}
-              <p
-                className="greeting-text-p subTitle"
-                style={{ color: theme.secondaryText }}
-              >
-                {greeting.subTitle}
-              </p>
-              <SocialMedia theme={theme} />
-              <div className="portfolio-repo-btn-div">
-                <Button
-                  text="⭐ Star Me On Github"
-                  newTab={true}
-                  href={greeting.portfolio_repository}
-                  theme={theme}
-                  className="portfolio-repo-btn"
-                />
-              </div>
-              {/* <div className="button-greeting-div">
-              <Button text="Contact me" href="#contact" />
-              <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
-            </div> */}
-            </div>
-          </div>
-          <div className="greeting-image-div">
-            {/* <img
-							alt="saad sitting on table"
-							src={require("../../assets/images/feelingProud.svg")}
-						></img> */}
-            <FeelingProud theme={theme} />
-          </div>
-        </div>
-      </div>
-    </Fade>
-  );
+	const theme = props.theme;
+	return (
+		<Fade bottom duration={2000} distance="40px">
+			<div className="greet-main" id="greeting">
+				<div className="greeting-main">
+					<div className="greeting-text-div">
+						<div>
+							<h1 className="main-name">Shakti Mishra</h1>
+							{greeting.nickname && (
+								<h2 className="greeting-nickname" style={{ color: theme.text }}>
+									( {greeting.nickname} )
+								</h2>
+							)}
+							<p
+								className="greeting-text-p subTitle"
+								style={{ color: theme.secondaryText }}
+							>
+								{greeting.subTitle}
+							</p>
+							<SocialMedia theme={theme} />
+							<div className="portfolio-repo-btn-div">
+								<Button
+									text="⭐ Star Me On Github"
+									newTab={true}
+									href={greeting.portfolio_repository}
+									theme={theme}
+									className="portfolio-repo-btn"
+								/>
+							</div>
+							{/* <div className="button-greeting-div">
+							<Button text="Contact me" href="#contact" />
+							<Button text="See my resume" newTab={true} href={greeting.resumeLink} />
+						</div> */}
+						</div>
+					</div>
+					<div className="greeting-image-div">
+						{/* 						alt="saad sitting on table"
+										src={require("../../assets/images/feelingProud.svg")}
+									> */}
+						<FeelingProud theme={theme} />
+					</div>
+				</div>
+			</div>
+		</Fade>
+	);
 }
